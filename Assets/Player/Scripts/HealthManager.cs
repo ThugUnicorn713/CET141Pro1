d04cca1df039289b66afb68842c88f01f7a7d3cb
ThugUnicorn713 <victoriaabeale@gmail.com>
2024-02-28 16:30:35 +0000

    healthbar (sort of)

--- Assets/Player/Scripts/HealthManager.cs
+++ Assets/Player/Scripts/HealthManager.cs
@@ -1,15 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HealthManager : MonoBehaviour
 {
-    [SerializeField]
-    float hitPoints = 100f;
+    [SerializeField] float hitMaxPoints = 100f;
+    float hitPoints;
+
+    public Slider healthSlider;
+
+    void Start()
+    {
+        hitPoints = hitMaxPoints;
+    }
 
     void Hit(float rawDamage)
     {
-        hitPoints = rawDamage;
+        hitPoints -= rawDamage;
+        SetHealthSlider();
 
         Debug.Log("OUCH:" + hitPoints.ToString());
 
@@ -17,6 +26,20 @@ public class HealthManager : MonoBehaviour
         {
             Debug.Log("TODO: GAME OVER - YOU DIED...sux");
         }
+    }
+
+    void SetHealthSlider()
+    {
+        if (healthSlider != null)
+        {
+            healthSlider.value = NormalisedHitPoints();
+
+        }
+    }
+
+    float NormalisedHitPoints()
+    {
+        return hitPoints / hitMaxPoints;
     }
    
 }
